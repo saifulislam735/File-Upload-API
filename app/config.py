46c0dad9ce985a8062fa442ea36bed8eb83b3065
saifulislam735 <mohimshihab735@gmail.com>
2025-03-04 13:38:23 +0600

    config_file

---
+++ app/config.py
@@ -0,0 +1,9 @@
+# Your MongoDB Atlas connection string from Step 5
+MONGODB_URI="mongodb+srv://bs23admin:SggIIjuqZ7eQetZQ@cluster0.6obomcw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+# Max file size (5MB in bytes)
+MAX_FILE_SIZE = 5 * 1024 * 1024
+
+# Allowed file types
+ALLOWED_TYPES = {"image/jpeg", "image/png", "application/pdf", "text/plain"}
